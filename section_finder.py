798258cb63584eeacef25684ae15c4d99c2adc95
tronschell <zoniixyt@gmail.com>
2021-07-02 20:06:45 -0400

    fixed some things

--- section_finder.py
+++ section_finder.py
@@ -8,28 +8,27 @@ def section_finder(document, section):
     # Use finditer to math the regex
     matches = regex.finditer(document['10-K'])
 
-
-    matches = regex.finditer(document['10-K'])
     # Create the dataframe
     test_df = pd.DataFrame([(x.group(), x.start(), x.end()) for x in matches])
 
     test_df.columns = ['item', 'start', 'end']
     test_df['item'] = test_df.item.str.lower()
 
-
+    # Replace any unnecessary characters or strings with a space or no space
     test_df.replace('&#160;',' ',regex=True,inplace=True)
     test_df.replace('&nbsp;',' ',regex=True,inplace=True)
     test_df.replace(' ','',regex=True,inplace=True)
     test_df.replace('\.','',regex=True,inplace=True)
     test_df.replace('>','',regex=True,inplace=True)
 
+    # Drop any duplicates and only keep the last one
     pos_dat = test_df.sort_values('start', ascending=True).drop_duplicates(subset=['item'], keep='last')
 
     # Set item as the dataframe index
     pos_dat.set_index('item', inplace=True)
 
 
-
+    # If the user inputs "item 7" then it returns item 7 and same thing for item 1a
     if section.lower() == "item 7":
         item_7_raw = document['10-K'][pos_dat['start'].loc['item7']:pos_dat['start'].loc['item7a']]
         item_7_content = BeautifulSoup(item_7_raw, 'lxml')
